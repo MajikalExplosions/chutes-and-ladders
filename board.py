81286dc4b0224bfd26023525b2c8dd0e684cf823
alisonsoong <57238372+alisonsoong@users.noreply.github.com>
2019-12-05 14:24:28 -0800

    Update board.py

--- board.py
+++ board.py
@@ -6,8 +6,7 @@ class Board(Frame):
     def __init__(self,master):
         '''Board()
         creates a new board for Chutes and Ladders'''
-        Frame.__init__(self,master,width=800,height=800,bg="white")  # set up as a Tk frame
-        self.pack_propagate(0)
+        Frame.__init__(self,master,bg="white",padx=15, pady=15)  # set up as a Tk frame
         self.grid()  # place the frame in the root window
 
         # variables
@@ -15,7 +14,7 @@ class Board(Frame):
 
         # First, the welcome screen:
         # create labels
-        self.welcomeLabel = Label(self,text = 'CHUTES AND LADDERS',font = "Verdana 30 bold", fg = "Cadet Blue")
+        self.welcomeLabel = Label(self,text = 'CHUTES AND LADDERS',font = "Verdana 30 bold", fg = "gray30")
         self.welcomeLabel.grid(row=0,columnspan=4)
         self.playerLabel = Label(self,text='How many players?',font=("Calibri"))
         self.playerLabel.grid(row=1,columnspan=4)
@@ -35,24 +34,29 @@ class Board(Frame):
 
         # Second: the actual game screen. Nothing is gridded yet (will be gridded in create_board)
         # title
-        self.title = Label(self,text = 'CHUTES AND LADDERS',font = "Verdana 30 bold", fg = "Cadet Blue")
+        self.title = Label(self,text = 'CHUTES AND LADDERS',font = "Verdana 30 bold", fg = "gray30")
 
         #game announcer/commentator
         self.text = StringVar()
-        self.comments = Label(self,height=37, width = 30,textvariable = self.text, font=("Calibri"), fg="White", bg = "Dark Grey")
+        self.comments = Label(self,anchor=S,wraplength = 200,borderwidth = 3, relief = GROOVE, height = 38, width = 30, textvariable = self.text, font=("Calibri"), fg="White", bg = "gray65")
         self.text.set("Welcome to Chutes and Ladders!\nThere are " + str(self.numPlayers) +" players playing in this game.\n")
         
         #game board
         self.boardGIF = PhotoImage(file="gameBoard.gif")
         self.board = Label(self,image=self.boardGIF)
 
+        #forward and backwards buttons
+        self.forward = Button(self,text='Forward',font = "Verdana 20 bold", fg = "gray65",state=DISABLED)
+        self.backward = Button(self,text = 'Backward',font = "Verdana 20 bold",state=DISABLED)
+        
     def set_num_players(self, x):
         '''set_num_players(self,x) -> takes in an integer (1,2,3,4)
             and stores it into numPlayers'''
         self.numPlayers = x
         self.playerLabel['text'] ='How many players?: '+str(x)
         self.next['state']=NORMAL
-        
+        self.text.set("Welcome to Chutes and Ladders!\nThere are " + str(self.numPlayers) +" players playing in this game.\n")
+
 
     def get_num_players(self):
         '''get_num_players(self) -> accessor method'''
@@ -65,58 +69,32 @@ class Board(Frame):
         list = self.grid_slaves()
         for l in list:
             l.destroy()
-        self.board.grid(row=1,column=0)
+        self.board.grid(row=1,columnspan=2)
         self.title.grid(row=0,columnspan=3)
         self.comments.grid(row = 1,column=3)
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")          #don't mind this haha. just trying to debug the comments :)
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("hi")
-        self.update_comments("woah")
-        
-        
-        
+        self.forward.grid(row = 2, column=1)
+        self.backward.grid(row = 2, column=0)
         
     def update_comments(self, x):
-        '''updates commentary text box'''
+        '''updates commentary text box. the commentary text box should
+        post messages like "player 1 slid down a slide!" or something similar.'''
         self.text.set(str(self.text.get()) + "\n" + str(x))
 
+    def enable_forward_button(self):
+        '''enables forward button'''
+        self.forward['state'] = NORMAL
 
+    def enable_backward_button(self):
+        '''enables backward button'''
+        self.backward['state'] = NORMAL
+
+    def disable_forward_button(self):
+        '''disables forward button'''
+        self.forward['state'] = DISABLED
+
+    def disable_backward_button(self):
+        '''disables backward button'''
+        self.backward['state'] = DISABLED
 
 
 class ChutesAndLadders:
